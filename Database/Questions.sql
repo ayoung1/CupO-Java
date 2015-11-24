8e0518f019c09ed0a1e28a645b7803ecf76e9541
bryce-fenske <bryce.fenske@gmail.com>
2015-11-24 12:22:32 -0800

    i4.1: DB
    Missing a few specs. Thanks Toad.

--- Database/Questions.sql
+++ Database/Questions.sql
@@ -10,16 +10,16 @@
 
 create table USERS
 (
-  user_name varchar unique not null,
-  user_password varchar
-);
+  user_name varchar (256)unique not null,
+  user_password varchar(256)
+)
 
 create table TRIVIA_QUESTIONS
 (
 	question_num int(10) unique not null
   on delete cascade on update cascade,
-	question_type varchar not null,
-	category varchar default null,
+	question_type varchar (20)not null,
+	category varchar (20) default null,
 	primary key (question_num)
 );
 
@@ -28,12 +28,12 @@ create table MULTIPLE_CHOICE
   foreign key worksin(question_num)
   references TRIVIA_QUESTIONS(question_num),
 	num_options int(1) default 2,
-	question varchar not null,
-	answer varchar not null,
-	option_1 varchar not null,
-	option_2 varchar,
-  option_3,
-	option_4 default null
+	question varchar (50) not null,
+	answer varchar (50) not null,
+	option_1 varchar (50)not null,
+	option_2 varchar (50),
+  option_3 varchar (50),
+	option_4 varchar (50)default null
 );
 
 create table TRUE_FALSE
